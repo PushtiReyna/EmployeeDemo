2c1a062d5365e4209be7cdb1055d06237287611e
Pushti Prajapati <pushti.prajapati@reynasolutions.com>
2023-08-09 17:01:57 +0530

    PP - Create Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeDBFirstDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
